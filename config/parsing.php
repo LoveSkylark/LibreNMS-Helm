8f3debf27efaf6a3da055b1f5acf45f0ba7746a9
LoveSkylark <s.stefan.sig@gmail.com>
2022-06-22 17:15:23 +0000

    example config added

---
+++ config/parsing.php
@@ -0,0 +1,7 @@
+<?php
+
+$config['customers_descr'][]         = 'c';
+$config['transit_descr'][]           = "t";
+$config['peering_descr'][]           = "p;
+$config['core_descr'][]              = "s";
+# $config['custom_descr'][]            = "something_made_up";
---
+++ config/syslog.php
@@ -0,0 +1,4 @@
+<?php
+
+$config['enable_syslog'] = 1;               # Enable Syslog
+$config['syslog_purge'] = 90;               # Number of days to retain
