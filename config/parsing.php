fe28ea9a0f4d81ea809d6b1ff8f8505e7b27ee68
Skylark <s.stefan.sig@gmail.com>
2022-06-23 10:57:25 +0000

    fixing LB

--- config/parsing.php
+++ config/parsing.php
@@ -3,5 +3,5 @@
 $config['customers_descr'][]         = 'c';
 $config['transit_descr'][]           = "t";
 $config['peering_descr'][]           = "p;
-$config['core_descr'][]              = "s";
+$config['core_descr'][]              = "i";
 # $config['custom_descr'][]            = "something_made_up";
