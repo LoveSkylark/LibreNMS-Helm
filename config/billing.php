253a5c12cf18fc610deaa1ef5c9d90292fd2a51a
Skylark <s.stefan.sig@gmail.com>
2022-06-24 18:45:42 +0000

    adding config backup

--- config/billing.php
+++ config/billing.php
@@ -1,5 +1,5 @@
 <?php
 
 $config['enable_billing'] = 1;              # Enable Billing
-$config['billing_data_purge'] = 12;         # Number of months to retain
+$config['billing_data_purge'] = 36;         # Number of months to retain
 $config['billing']['95th_default_agg'] = 1; # Set aggregate 95th as default
