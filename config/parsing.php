0716bb817cc7c7f1662cb37338c53ba4126dd51a
LoveSkylark <s.stefan.sig@gmail.com>
2022-06-27 23:05:33 +0000

    adding oxidized

--- config/parsing.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$config['customers_descr'][]         = 'c';
-$config['transit_descr'][]           = "t";
-$config['peering_descr'][]           = "p;
-$config['core_descr'][]              = "i";
-# $config['custom_descr'][]            = "something_made_up";
--- config/syslog.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$config['enable_syslog'] = 1;               # Enable Syslog
-$config['syslog_purge'] = 90;               # Number of days to retain
