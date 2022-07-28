fa5e543312b43f01980a66a40c1dd59850932c1b
Skylark <s.stefan.sig@gmail.com>
2022-07-28 15:38:01 +0000

    Update full-setup.sh

--- full-setup.sh
+++ full-setup.sh
@@ -12,9 +12,10 @@
 #wget https://raw.githubusercontent.com/LoveSkylark/dotfiles/main/Linux/.bash_aliases ~
 #exec "$SHELL"
 
-#echo "Downloading and installing K3S"
-#export INSTALL_K3S_BIN_DIR=/usr/local/bin
-#curl -sfL 'https://get.k3s.io' | sh -
+echo "Downloading and installing K3S"
+export INSTALL_K3S_BIN_DIR=/usr/local/bin
+curl -sfL 'https://get.k3s.io' | sh -
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
 echo "Downloading and installing HELM"
-curl -sfL 'https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3' | sh -
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
