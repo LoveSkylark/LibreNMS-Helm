e1e280e95aac78b59cafc6706121320bfddc5a29
Skylark <s.stefan.sig@gmail.com>
2022-07-28 16:27:29 +0000

    Update full-setup.sh

--- full-setup.sh
+++ full-setup.sh
@@ -5,7 +5,7 @@
 #sudo apt update && sudo apt upgrade -y
 
 #echo "############## Installing basic tools ##############"
-#apt install vim
+#apt install vim git
 
 #echo "############## Adding alias for KUBE & HELM##############"
 #mv ~/.bash_aliases ~/.bash_aliases.backup
@@ -20,6 +20,30 @@ export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 echo "Downloading and installing HELM"
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
+echo "############## Fetching helm chart ##############"
+git clone https://github.com/LoveSkylark/LibreNMS-Helm.git
+mv ./example.values.yaml.example values.yaml
+
 echo "############## seting up Libre cluster ##############"
-wget https://raw.githubusercontent.com/LoveSkylark/LibreNMS-Helm/main/example/values.yaml.example
-mv values.yaml.example values.yaml
+
+echo "usfeull short cuts:"
+echo "    k=kubectl"
+echo "       kl                            // list all resoucess in namsepace"
+echo "       kall                          // list all resoucess in all namsepaces"
+echo "       kn <namespace>                //switch to another namespace"
+echo "       knd <defaul_namespace>        //switch to back to default"
+echo "       kbash <pda_name>              // jump into a pod"
+echo ""
+echo "    h=helm"
+echo "       hl                            // list all releases in namsepace"
+echo "       hall                          // list all releases in all namsepaces"
+echo "       hin                           // helm install"
+echo "       hup                           // helm upgrade"
+echo "       hun                           // helm uninstall"
+echo ""
+echo ""
+echo "               next step:"
+echo "                     1. configure your cluster by editing values.yaml (can be re-named to <corportation>.yaml)" 
+echo "                     2. build the cluster using 'hin <clustername> ./librenms vlaues.yaml (or  <corportation>.yaml)"
+echo ""
+echo ""
